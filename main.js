ca047dc073f3a7af05ae390405a8d8254c42ee17
toan2013dn <tofbest267@gmail.com>
2022-06-06 14:28:22 +0700

    table

--- main.js
+++ main.js
@@ -3,18 +3,18 @@ import Swal from 'sweetalert2/dist/sweetalert2.js'
 
 import 'sweetalert2/src/sweetalert2.scss'
 
-const button = document.getElementById('button')
+const button = document.getElementById('remove')
 button.addEventListener("click", function(){
     Swal.fire({
-        title: 'Do you want to save the changes?',
+        title: 'Do you want to remove this task?',
         showDenyButton: true,
         showCancelButton: true,
-        confirmButtonText: 'Save',
-        denyButtonText: `Don't save`,
+        confirmButtonText: 'Yes',
+        denyButtonText: `No`,
       }).then((result) => {
         /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
-          Swal.fire('Saved!', '', 'success')
+          Swal.fire('Removed!', '', 'success')
         } else if (result.isDenied) {
           Swal.fire('Changes are not saved', '', 'info')
         }
