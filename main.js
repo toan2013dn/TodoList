1430d18b6a36b315f18f7af2f33da4547d673f02
TienThinh-fe <thinhhtde150233@fpt.edu.vn>
2022-06-08 15:13:33 +0700

    fea: fetch template

--- main.js
+++ main.js
@@ -1,72 +1,69 @@
 import "@styles/main.scss";
+import axios from "axios";
 import Swal from "sweetalert2/dist/sweetalert2.js";
 
 import "sweetalert2/src/sweetalert2.scss";
 
-const buttonRemove = document.getElementById("removebtn");
-buttonRemove.addEventListener("click", function () {
-  Swal.fire({
-    title: "Do you want to remove this task?",
-    showDenyButton: true,
-    showCancelButton: true,
-    confirmButtonText: "Yes",
-    denyButtonText: `No`,
-  }).then((result) => {
-    /* Read more about isConfirmed, isDenied below */
-    if (result.isConfirmed) {
-      Swal.fire("Removed!", "", "success");
-    } else if (result.isDenied) {
-      Swal.fire("Changes are not saved", "", "info");
-    }
-  });
-});
+// const buttonRemove = document.getElementById("removebtn");
+// buttonRemove.addEventListener("click", function () {
+//   Swal.fire({
+//     title: "Do you want to remove this task?",
+//     showDenyButton: true,
+//     showCancelButton: true,
+//     confirmButtonText: "Yes",
+//     denyButtonText: `No`,
+//   }).then((result) => {
+//     /* Read more about isConfirmed, isDenied below */
+//     if (result.isConfirmed) {
+//       Swal.fire("Removed!", "", "success");
+//     } else if (result.isDenied) {
+//       Swal.fire("Changes are not saved", "", "info");
+//     }
+//   });
+// });
 
-const button = document.getElementById("addTask");
-button.addEventListener("click", async function () {
-  const { value: formValues } = await Swal.fire({
-    title: "Add Task",
-    html:
-      '<label for="taskname">Task:</label>' +
-      '<input id="swal-input1" class="swal2-input">' +
-      '<label for="status">Status:</label>' +
-      '<input id="swal-input2" class="swal2-input">',
-    focusConfirm: false,
-    preConfirm: () => {
-      return [
-        document.getElementById("swal-input1").value,
-        document.getElementById("swal-input2").value,
-      ];
-    },
-  });
+// const button = document.getElementById("addTask");
+// button.addEventListener("click", async function () {
+//   const { value: formValues } = await Swal.fire({
+//     title: "Add Task",
+//     html:
+//       '<label for="taskname">Task:</label>' +
+//       '<input id="swal-input1" class="swal2-input">' +
+//       '<label for="status">Status:</label>' +
+//       '<input id="swal-input2" class="swal2-input">',
+//     focusConfirm: false,
+//     preConfirm: () => {
+//       return [
+//         document.getElementById("swal-input1").value,
+//         document.getElementById("swal-input2").value,
+//       ];
+//     },
+//   });
 
-  if (formValues) {
-    Swal.fire(JSON.stringify(formValues));
-  }
-});
+const getAllTasks = async () => {
+  const responseData = await axios.get("http://localhost:3001/api/task");
+  const firstTask = responseData.data.data[4];
+  const item = `
+  <div  class="task--item">
+          <div class="task--item__checkbox">
+            <input id="checked4" type="checkbox" checked />
+          </div>
+          <div class="task--item__name"><label for="checked4">${
+            firstTask.name
+          }</label></div>
+          <div class="task--item__status">
+            <div class="${firstTask.status.toLowerCase()}">${
+    firstTask.status
+  }</div>
+          </div>
+          <div id="removebtn" class="task--item__remove">
+            <img src="./source/assets/delete.png" alt="" />
+          </div>
+        </div>
+  `;
+  const grid = document.querySelector(".task--grid");
+  console.log(grid);
+  grid.innerHTML += item;
+};
 
-var postApi = 'https://jsonplaceholder.typicode.com/todos';
-
-function start() {
-  getTasks(renderTasks);
-}
-
-// Functions
-function getTasks(callback) {
-  fetch(postApi)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(callback)
-    .catch(function (err) {
-      console.log(err);
-    });
-}
-
-function renderTasks(tasks) {
-  var todoTask = document.querySelector("#task--item");
-  var htmls = tasks.map(function (task) {
-    return `
-    `
-  });
-  todoTask.innerHTML = htmls.join('');
-}
+getAllTasks();
